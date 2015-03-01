9305ad31366300cb5606a9ef3945992796f3ba93
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-01 18:33:55 -0500

    got the blue board to show up, yayls

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,21 @@
-// Replace this with your code...
+//Lab 5 - Ms Pacman
+//Javascript
+//Lisa Fukushima
+//March 3, 2015
+
+var document;
+function init() {
+	var game_cnvs = document.getElementById('game_canvas');
+	if (game_cnvs.getContext) {
+		var game_ctx = game_cnvs.getContext('2d');
+		var game_img = new Image();
+		
+		game_img.src = "./pacman10-hp-sprite.png";
+		game_img.addEventListener("load", function() {
+			//blue board
+			game_ctx.drawImage(game_img, 322, 0, 464, 138, 0, 0, 464, 138);
+			//Ms PacMan
+	//		game_ctx.drawImage(game_img, 322, 0, 464, 138, 0, 0, 464, 138);
+		}, false)
+	}
+}
