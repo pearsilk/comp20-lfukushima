40b77c9411c34e60f8f36fefc5c4b9200ca3f380
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-01 19:16:05 -0500

    fixed up minor errors found by JSLint

--- mspacman/game.js
+++ mspacman/game.js
@@ -11,11 +11,11 @@ function init() {
 		var game_img = new Image();
 		
 		game_img.src = "./pacman10-hp-sprite.png";
-		game_img.addEventListener("load", function() {
+		game_img.addEventListener("load", function () {
 			//blue board
 			game_ctx.drawImage(game_img, 322, 0, 464, 138, 0, 0, 464, 138);
 			//Ms PacMan
 			game_ctx.drawImage(game_img, 82, 23, 15, 15, 282, 55, 15, 15);
-		}, false)
+		}, false);
 	}
 }
