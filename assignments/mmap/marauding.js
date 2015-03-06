20654beca8f051ecfc2808187e7e2e9570de3fe4
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-06 18:21:07 -0500

    trying to load simple map

--- assignments/mmap/marauding.js
+++ assignments/mmap/marauding.js
@@ -1,1 +1,14 @@
-// Empty JS file for now . . .
+// Assignment 2 - Marauder's Map
+// COMP 20 - Spring 2015
+// Lisa Fukushima
+// March 6, 2015
+
+function unfoldMap() {
+	var tufts = new google.maps.LatLng(42.407484, -71.119023);
+	var mapOptions = {
+		center: tufts,
+		zoom: 8
+	};
+
+	var map = new google.maps.Map(document.getElementById('marauders-map'), mapOptions);
+}
