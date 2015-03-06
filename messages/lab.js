695617befba5652c3cebb18bd7ca748b31aba601
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-06 01:17:44 -0500

    clean up formatting

--- messages/lab.js
+++ messages/lab.js
@@ -13,11 +13,13 @@ function parse() {
 function parseData() {
 	if (request.readyState == 4 && request.status == 200) {
 		console.log("data successfully retrieved");
-		messages = JSON.parse(request.response);
+		var messages = JSON.parse(request.response);
 		var msg_style = "";
 		for (elem in messages) {
-			msg_style += '<p><span class = "id">' + messages[elem]['id'] + '</span><span class = "content">' + messages[elem]['content']
-				     + '</span><span class = "username">' +  messages[elem]['username'] + '</span></p>' 
+			msg_style += '<p><span class = "id">' + messages[elem]['id']
+				     + '</span><span class = "content">' + messages[elem]['content']
+				     + '</span><span class = "username">' +  messages[elem]['username']
+				     + '</span></p>';
 		}
 		msg_block = document.getElementById("messages");
 		msg_block.innerHTML = msg_style;
@@ -25,7 +27,8 @@ function parseData() {
 	} else if (request.readyState == 4 && request.status != 200) {
 		console.log("ERROR: ready state = ", request.readyState, " status code = ", request.status);
 		var err_msg_block = document.getElementById("messages");
-		var err_msg = "Oh no! An error occurred . . . <p>ready state = " + request.readyState + "<p>status code = " + request.status;
+		var err_msg = "Oh no! An error occurred . . . <p>ready state = " + request.readyState
+			      + "<p>status code = " + request.status;
 		err_msg_block.innerHTML = err_msg;
 	}
 }
