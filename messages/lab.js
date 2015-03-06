d3f09eb6cde6c864ab36ba1fcd4e9a403a6531b7
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-05 23:53:27 -0500

    added basic skeleton to lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,15 @@
-// Your JavaScript goes here...
+// Lab 6 - Messages
+// Lisa Fukushima
+// COMP 20 - Spring 2015
+
+var request;
+function parse() {
+	request = new XMLHttpRequest();
+	request.open("GET", "./data.json", true);
+	request.onreadystatechange = parseData;
+	request.send();
+}
+
+function parseData {
+	console.log("checking for errors");
+}
