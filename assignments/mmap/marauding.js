0f430f744f6578a40ae1c6bd7ac23cdc5ce8dd09
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-07 01:05:26 -0500

    trying to figure out why I have unexpected string error

--- assignments/mmap/marauding.js
+++ assignments/mmap/marauding.js
@@ -40,11 +40,12 @@ function findMyPos() {
 }
 
 /* 'success' callback function for getCurrentPosition() */
+var my_lat, my_lng;
 function defineMyPos(my_pos) {
 	my_lat = my_pos.coords.latitude;
 	my_lng = my_pos.coords.longitude;
-	var msg = "My lat: " + my_lat " and my lng: " + my_lng;
-	console.log(msg);
+	//var msg = "My lat: " + my_lat " and my lng: " + my_lng;
+	console.log("hello");
 }
 
 
