611b2d16366e6514c2a25d8fbbce2911b3b85d57
Lisa Fukushima <pearsilk.lisa@gmail.com>
2015-03-09 22:34:28 -0400

    rearranged code a little

--- labs/messages/lab.js
+++ labs/messages/lab.js
@@ -1,18 +1,20 @@
 // Lab 6 - Messages
 // Lisa Fukushima
 // COMP 20 - Spring 2015
+// March 9, 2015
 
 var request;
 function parse() {
+	var req_url = "./data.json"; // local file
+	//var req_url = "http://messagehub.herokuapp.com/messages.json"; // remote file
 	request = new XMLHttpRequest();
-	request.open("GET", "./data.json", true);
+	request.open("GET", req_url, true);
 	request.onreadystatechange = parseData;
 	request.send();
 }
 
 function parseData() {
 	if (request.readyState == 4 && request.status == 200) {
-		console.log("data successfully retrieved");
 		var messages = JSON.parse(request.response);
 		var msg_style = "";
 		for (elem in messages) {
@@ -23,7 +25,6 @@ function parseData() {
 		}
 		msg_block = document.getElementById("messages");
 		msg_block.innerHTML = msg_style;
-		console.log("data successfully parsed");
 	} else if (request.readyState == 4 && request.status != 200) {
 		console.log("ERROR: ready state = ", request.readyState, " status code = ", request.status);
 		var err_msg_block = document.getElementById("messages");
